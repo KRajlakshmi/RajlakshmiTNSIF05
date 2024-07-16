07f9e83e52cbc89ea83734230d7c720ea151ed0a
Rajlakshmi Kashyap <rajlakshmikashyap@gmail.com>
2024-07-16 23:51:31 +0530

    Hello World program

---
+++ TNSCOREJAVA/src/tns/corejava/HelloWorld.java
@@ -0,0 +1,11 @@
+package tns.corejava;
+//Demo of printing a msg on console
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Everyone!!");
+
+	}
+
+}

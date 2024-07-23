e8408aa6624028aeafa8e2ce6faa557bb21dd5a6
Rajlakshmi Kashyap <rajlakshmikashyap@gmail.com>
2024-07-23 10:20:03 +0530

    saved

---
+++ TNSCOREJAVA/src/com/dayone/firstprog/DataTypeDemo.java
@@ -0,0 +1,18 @@
+package com.dayone.firstprog;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		int value = 9/2;
+		float value1 = 101f/61f;
+		double value2 = 10d/6d;
+		System.out.println("Value is : "+value);
+		System.out.println("Value 1  is : "+value1);
+		System.out.println("Value 2 is " +value2);
+		
+		int marker = 512;
+		double percentage = marker*0.45f;
+		System.out.println("Percentage : "+percentage);
+	}
+
+}
---
+++ TNSCOREJAVA/src/com/dayone/firstprog/IdentifierDemo.java
@@ -0,0 +1,20 @@
+package com.dayone.firstprog;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Invalid Identifer
+		 //int number 1 =23;
+		//int for = 13;
+		
+		//valid variable
+		 int $sum = 23;
+		 System.out.println("Value of the number is :" + $sum);
+		  String Name = "Rajlaskhmi";
+		  
+		  System.out.println("Name is :"+ Name);
+				
+	}
+
+}
